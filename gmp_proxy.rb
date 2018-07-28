078596e6a3613cdb1abbc5782e1f79044bcb2486
Travis Sperry <director@mathplusacademy.com>
2018-07-28 13:43:47 -0400

    Update additional URL redirects

--- gmp_proxy.rb
+++ gmp_proxy.rb
@@ -15,3 +15,23 @@ end
 get '/learn' do
   redirect "https://www.globalmathproject.org/about"
 end
+
+get '/gmw' do
+  redirect "https://www.explodingdots.org/"
+end
+
+get '/ambassadors' do
+  redirect "https://www.globalmathproject.org/ambassadors"
+end
+
+get '/gmw/teaching_guides' do
+  redirect "https://www.globalmathproject.org/lessons"
+end
+
+get '/gmw/learn-about-exploding-dots' do
+  redirect "https://www.explodingdots.org/faq"
+end
+
+get '/personal-polynomial' do
+  redirect "https://www.globalmathproject.org/personal-polynimial"
+end
